b87e872adaf5a60e10d017cf094b58c7d0b77667
20tsange1 <20tsange1@gmail.com>
2022-10-27 19:10:33 +0100

    Trying Stick to Wall

--- Comp0002/Coursework 1/Robot.c
+++ Comp0002/Coursework 1/Robot.c
@@ -4,95 +4,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "graphics.h"
+#include <time.h>
 
 #define CanvasWidth 480
 #define CanvasHeight 480
 
-
-void drawMaze(int maze[12][12]) {
+void drawMaze(int maze[12][12])
+{
     background();
     setColour(black);
 
-    for (int y = 0; y < 12; y++) {
-        for (int x = 0; x < 12; x++) {
+    for (int y = 0; y < 12; y++)
+    {
+        for (int x = 0; x < 12; x++)
+        {
 
-            if (maze[y][x] == 1) {
+            if (maze[y][x] == 1)
+            {
                 setColour(white);
             }
-            else {
-                setColour(black);   
+            else
+            {
+                setColour(black);
             }
-            fillRect(x*40, y*40, x*40 + 40, y*40 + 40);
+            fillRect(x * 40, y * 40, x * 40 + 40, y * 40 + 40);
             setColour(black);
-            drawRect(x*40, y*40, x*40 + 40, y*40 + 40);
-            
+            drawRect(x * 40, y * 40, x * 40 + 40, y * 40 + 40);
         }
     }
 }
 
-void generateMaze(int maze[12][12], int mazeStartEnd[4]) {
+void resetMaze(int maze[12][12])
+{
 
-    //make maze using probablistic approach, skewed to generate longer lines
+    for (int y = 0; y < 12; y++)
+    {
+        for (int x = 0; x < 12; x++)
+        {
+            maze[y][x] = 0;
+        }
+    }
+}
 
-    for (int y = 0; y < 12; y++) {
-        for (int x = 0; x < 12; x++) {
-            if (rand() % 10 <= 4) {
+void generateMaze(int maze[12][12], int mazeStartEnd[4])
+{
+
+    srand(time(NULL));
+
+    // make maze using probablistic approach, skewed to generate longer lines
+    for (int i = 0; i < 12; i++)
+    {
+        maze[0][i] = 0;
+    }
+
+    maze[1][0] = 1;
+
+    for (int y = 1; y < 12; y++)
+    {
+        for (int x = 1; x < 11; x++)
+        {
+            // add so that it can be generated on the left too. (if all 4 sides empty, extend)
+            // add so that it can extend in any direction from 1 to some n length (Needs to include going left and going up)
+            if ((((maze[y][x - 1] == 1) && (maze[y + 1][x] == 0 && maze[y - 1][x] == 0)) || ((maze[y - 1][x] == 1) && (maze[y][x + 1] == 0 && maze[y][x - 1] == 0)) && (rand() % 10 <= 8)))
+            {
                 maze[y][x] = 1;
-            }
-            else {
-                maze[y][x] = 0;
-            }
 
-            // saying that the maze ends there
-            if (x == 11) {
-                mazeStartEnd[2] = x;
-                mazeStartEnd[3] = y;
-                break;
+                for (int i = x; i < (rand() % 5); i++)
+                {
+                    if (maze[y - 1][i] == 0 && maze[y + 1][i] == 0)
+                    {
+                        if (x < 11)
+                        {
+                            maze[y][i] = 1;
+                        }
+                    }
+                }
+                for (int i = y; i < (rand() % 5); i++)
+                {
+                    if (maze[i][x - 1] == 0 && maze[i][x + 1] == 0)
+                    {
+                        if (x < 11)
+                        {
+                            maze[i][x] = 1;
+                        }
+                    }
+                }
+
+                // saying that the maze ends there
+
+                if (y == 11)
+                {
+                    mazeStartEnd[2] = x;
+                    mazeStartEnd[3] = y;
+                    break;
+                }
             }
         }
     }
 
     mazeStartEnd[0] = 0;
     mazeStartEnd[1] = 1;
-
-
 }
 
-void drawRobot(int x, int y, int direction) {
+void drawRobot(int x, int y, int direction)
+{
     foreground();
     setColour(blue);
-    for (int i = 0; i < 40; i++) {
+    for (int i = 0; i < 40; i++)
+    {
         sleep(10);
         clear();
-
-        switch (direction) {
-
-            case (-1):
-                fillArc(x * 40 + 5, y * 40 + 5, 30, 30, 0, 360);
-                break;
-
-            case (1):
-                fillArc(x * 40 + 5, (y-1) * 40 + 5 + i, 30, 30, 0, 360);
-                break;
-
-            case (2):
-                fillArc(x * 40 + 5, (y+1) * 40 + 5 + i, 30, 30, 0, 360);
-                break;
-
-            case (3):
-                fillArc((x-1) * 40 + 5 + i, y * 40 + 5, 30, 30, 0, 360);
-                break;
-
-            case (4):
-                fillArc((x+1) * 40 + 5 + i, y * 40 + 5, 30, 30, 0, 360);
-                break;
-
-        }
+        fillArc(x * 40 + 5, y * 40 + 5, 30, 30, 0, 360);
     }
-    
 }
 
-void endFound() {
+void endFound()
+{
     clear();
     background();
     clear();
@@ -103,81 +130,142 @@ void endFound() {
     foreground();
     setColour(white);
     drawString("Robot has solved the maze", 150, 240);
-
 }
 
 // Example basic maze ( 0 = black 1 = white)
-//int maze[12][12] = { {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} , {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1} };
-
-
+// int maze[12][12] = { {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} , {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1} };
 
 // Robot should only be able to see in 4 directions, hence see maze[i][i], maze[i+1][i], maze[i-1][i], maze[i][i+1], maze[i][i-1]
 
-int main() {
+int main()
+{
 
-    setWindowSize(CanvasWidth,CanvasHeight);
+    setWindowSize(CanvasWidth, CanvasHeight);
 
-    int maze[12][12] = { {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} , {1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} };
-    // Array containing start and end of maze
+    // int maze[12][12] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    //  Array containing start and end of maze
     int mazeStartEnd[4];
 
-    //int maze[12][12];
+    mazeStartEnd[0] = 0;
+    mazeStartEnd[1] = 1;
 
-    //generateMaze(maze, mazeStartEnd);
+    // mazeStartEnd[2] = 11;
+    // mazeStartEnd[3] = 10;
+
+    // change to calloc, so memory gets allocated and bits get changed to 0. This prevents old memory from being accessed.
+    // int maze[12][12] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    int maze[12][12];
+    
+    resetMaze(maze);
+
+    generateMaze(maze, mazeStartEnd);
 
     drawMaze(maze);
 
-    int robotX = 0;
-    int robotY = 1;
+    int robotX = mazeStartEnd[0];
+    int robotY = mazeStartEnd[1];
 
-    int lastRobotX = 0;
-    int lastRobotY = 0;
+    int lastRobotX = mazeStartEnd[0];
+    int lastRobotY = mazeStartEnd[1];
 
-    int direction = -1; // Up = 1 Down = 2 Right = 3 Left = 4
+    int currentDirection = 1; // Up Down = 1 Left Right = -1
 
-    while ( (robotY != mazeStartEnd[2]) || (robotX != mazeStartEnd[3]) ) {
+    int direction = 3; // Up = 0 Down = 1 Right = 2 Left = 3
+
+    while ((robotX != mazeStartEnd[2]) || (robotY != mazeStartEnd[3]))
+    {
         drawRobot(robotX, robotY, direction);
 
         // Condition should be so that it moves in a new direction continously.
 
-        if ( (maze[robotY + 1][robotX] == 1) && ( (( robotY + 1 ) != lastRobotY) && ( robotX != lastRobotX) ) ) {
-            robotY += 1;
+        // Take rights until at dead ends, then take lefts
 
-            lastRobotY = robotY - 1;
+        switch (direction % 4)
+        {
+        case (0):
 
-            direction = 1;
+            if ((maze[robotY + 1][robotX] == 1) && (maze[robotY][robotX + 1] == 0))
+            {
+                lastRobotY = robotY;
+                robotY += 1;
+            }
+            else if (maze[robotY][robotX + 1] == 1)
+            {
+                lastRobotX = robotX;
+                robotX += 1;
+                direction = 1;
+            }
+            else
+            {
+                direction++;
+            }
+
+            break;
+
+        case (1):
+
+            if ((maze[robotY][robotX + 1] == 1) && (maze[robotY - 1][robotX] == 0))
+            {
+                lastRobotX = robotX;
+                robotX += 1;
+            }
+            else if (maze[robotY - 1][robotX] == 1)
+            {
+                lastRobotY = robotY;
+                robotY -= 1;
+                direction = 2;
+            }
+            else
+            {
+                direction++;
+            }
+
+            break;
+
+        case (2):
+
+            if ((maze[robotY - 1][robotX] == 1) && (maze[robotY][robotX - 1] == 0))
+            {
+                lastRobotY = robotY;
+                robotY -= 1;
+            }
+            else if (maze[robotY][robotX - 1] == 1)
+            {
+                lastRobotX = robotX;
+                robotX -= 1;
+                direction = 3;
+            }
+            else
+            {
+                direction++;
+            }
+
+            break;
+
+        case (3):
+
+            if ((maze[robotY][robotX - 1] == 1) && (maze[robotY + 1][robotX] == 0))
+            {
+                lastRobotX = robotX;
+                robotX -= 1;
+            }
+            else if (maze[robotY + 1][robotX] == 1)
+            {
+                lastRobotY = robotY;
+                robotY += 1;
+                direction = 0;
+            }
+            else
+            {
+                direction++;
+            }
+
+            break;
         }
 
-        else if ( (maze[robotY - 1][robotX] == 1) && ( ( ( robotY - 1 ) != lastRobotY) && ( robotX != lastRobotX) ) ) {
-            robotY -= 1;
-             
-            lastRobotY = robotY + 1;
-
-            direction = 2;
-        }
-
-        else if ( (maze[robotY][robotX + 1] == 1) && ( ( ( robotX + 1 ) != lastRobotX) && ( robotY != lastRobotY) ) ) {
-            robotX += 1;
-
-            lastRobotX = robotX - 1;
-
-            direction = 3;
-        }
-
-        else if ( (maze[robotY][robotX - 1] == 1) && ( ( ( robotX - 1 ) != lastRobotX) && ( robotY != lastRobotY) ) ) {
-            robotX -= 1;
-
-            lastRobotX = robotX + 1;
-
-            direction = 4;
-        }
-
-
-        
-        printf("%i, %i\n", robotX, robotY);
-
+        printf("%i, %i -- %i, %i -- %i\n", robotX, robotY, lastRobotX, lastRobotY, direction);
     }
-
 
     // end sequence as loop ends when maze end is found but not travelled to.
     drawRobot(robotX, robotY, direction);
@@ -185,7 +273,6 @@ int main() {
     drawRobot(robotX, robotY, direction);
 
     endFound();
-
 
     return 0;
 }
